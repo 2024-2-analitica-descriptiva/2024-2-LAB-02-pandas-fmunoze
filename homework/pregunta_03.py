bff48f234935926d40db15f94c209a1dbdad4bb6
Felipe Muñoz <fmunoze@unal.edu.co>
2025-01-13 00:32:15 -0500

    T1

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -5,19 +5,27 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_03():
-    """
-    ¿Cuál es la cantidad de registros por cada letra de la columna `c1` del
-    archivo `tbl0.tsv`?
 
-    Rta/
-    c1
-    A     8
-    B     7
-    C     5
-    D     6
-    E    14
-    Name: count, dtype: int64
+    df = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    value_counts = df.iloc[:, 1].value_counts().sort_index()
+    return value_counts
 
-    """
+
+
+"""
+¿Cuál es la cantidad de registros por cada letra de la columna `c1` del
+archivo `tbl0.tsv`?
+
+Rta/
+c1
+A     8
+B     7
+C     5
+D     6
+E    14
+Name: count, dtype: int64
+
+"""
